adc5d557c566bbe17d9cb875c5859b36f739e123
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-21 10:37:34 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/ecodation_2026_fullstack_springreact_5/Ecodation2026FullStackSpringReact5Application.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.ecodation_2026_fullstack_springreact_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecodation2026FullStackSpringReact5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecodation2026FullStackSpringReact5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamitmizrak/ecodation_2026_fullstack_springreact_5/Ecodation2026FullStackSpringReact5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.ecodation_2026_fullstack_springreact_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecodation2026FullStackSpringReact5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
